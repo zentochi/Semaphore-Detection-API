c549915c4e4eb0ef7900ff93919481f059780993
zentochi <danuarta241@gmail.com>
2023-12-13 14:52:15 +0700

    chore: capitalize `predicted_class` character

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ model_path = 'model_ml.h5'
 model = load_model(model_path)
 
 # Semaphore classes
-classes = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-           'w', 'x', 'y', 'z']
+classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+           'W', 'X', 'Y', 'Z']
 
 
 # Function to preprocess the image
